692ec803f9cc19facda97f669b22c1769db1b9e9
toBeOfUse <comingaflame@gmail.com>
2022-01-20 21:44:45 -0500

    get basic ScheduledPost timing logic working

--- models.py
+++ models.py
@@ -1,5 +1,9 @@
+from datetime import datetime, timedelta
 import logging
-from sqlalchemy import create_engine, Column, Integer, BigInteger, String, Float, Sess
+from typing import Optional
+from zoneinfo import ZoneInfo
+
+from sqlalchemy import create_engine, Column, Integer, BigInteger, String, Float
 from sqlalchemy.orm import registry
 
 logging.basicConfig()
@@ -12,11 +16,13 @@ engine = create_engine("sqlite+pysqlite:///data/schedule.db", future=True)
 mapper_registry = registry()
 Base = mapper_registry.generate_base()
 
+tz = ZoneInfo("America/New_York")
+
 class ScheduledPost(Base):
     __tablename__ = "schedule"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    guild_id = Column(BigInteger, unique=True, nullable=False)
+    guild_id = Column(BigInteger, nullable=False)
     channel_id = Column(BigInteger, nullable=False)
     current_session = Column(String)
     timing = Column(Float, nullable=False)
@@ -24,5 +30,62 @@ class ScheduledPost(Base):
     def __repr__(self):
         return (f"Posting in channel {self.channel_id} (guild {self.guild_id}) at "+
             f"{self.timing} hours. Current session ID is {self.current_session}.")
+    
+    def get_next_time(self, starting_from: Optional[datetime]=None) -> datetime:
+        if starting_from is None:
+            base = datetime.now(tz=tz)
+        else:
+            base = starting_from
+        nowHours = base.hour+base.minute/60+base.second/3600
+        if nowHours >= self.timing:
+            base += timedelta(days=1)
+        return datetime(
+            year=base.year, 
+            month=base.month, 
+            day=base.day, 
+            hour=int(self.timing), 
+            minute=int((self.timing%1)*60),
+            tzinfo=tz
+        )
+    
+    def seconds_until_next_time(
+        self,
+        starting_from: Optional[datetime]=None
+    ) -> float:
+        base = starting_from or datetime.now()
+        return (
+            self.get_next_time(starting_from).astimezone(ZoneInfo("UTC")) -
+            base.astimezone(ZoneInfo("UTC"))
+        ).total_seconds()
+        
 
 Base.metadata.create_all(engine)
+
+if __name__ == "__main__":
+    print("Current Time:")
+    print(datetime.now(tz=tz))
+    test = ScheduledPost(guild_id=-1, channel_id=-1, timing=7)
+    print("7:00 Eastern:")
+    print(test.get_next_time())
+    print("which is in:")
+    print(test.get_next_time()-datetime.now(tz=tz))
+
+    print("From March 12th, 2022:")
+    dst_base = datetime(2022, 3, 12, 7, tzinfo=tz)
+    dst_result = test.get_next_time(dst_base)
+    print(dst_result)
+    print("which is this much later:")
+    print(timedelta(seconds=test.seconds_until_next_time(dst_base)))
+
+    print("From 7am on November 5th, 2022:")
+    dst_base = datetime(2022, 11, 5, 7, tzinfo=tz)
+    print(test.get_next_time(dst_base))
+    print("which is this much later:")
+    print(timedelta(seconds=test.seconds_until_next_time(dst_base)))
+
+    print("or, from 3am on November 5th:")
+    test = ScheduledPost(guild_id=-1, channel_id=-1, timing=3)
+    dst_base = datetime(2022, 11, 5, 3, tzinfo=tz)
+    print(test.get_next_time(dst_base))
+    print("which is this much later:")
+    print(timedelta(seconds=test.seconds_until_next_time(dst_base)))
